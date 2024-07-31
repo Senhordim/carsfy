bfac0a5a0ebd346ad37d59c2e53ef73bd65524bd
Diego Collares <diego.collares@gmail.com>
2024-07-31 19:32:42 -0300

    Image car admin

--- cars/admin.py
+++ cars/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.html import escape
 from cars.models import Car, Brand
 
 # Register your models here.
+
 
 class BrandAdmin(admin.ModelAdmin):
   list_display = ('name',)
@@ -12,7 +15,13 @@ class BrandAdmin(admin.ModelAdmin):
 
 
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+
+    def image_tag(self, obj):
+      return format_html('<img src="%s" width="100" height="60" />' % escape(obj.photo.url))
+
+    image_tag.short_description = 'Image'
+
+    list_display = ('image_tag', 'model', 'brand', 'factory_year', 'model_year', 'value')
     list_filter = ('model', 'brand', 'factory_year', 'model_year', 'value')
     search_fields = ('model', 'brand', 'factory_year', 'model_year', 'value')
     ordering = ['model', 'brand', 'factory_year', 'model_year', 'value']
