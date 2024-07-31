3f2823bcd1795fb565c01721a46ef9068a787af1
Diego Collares <diego.collares@gmail.com>
2024-07-31 16:01:31 -0300

    Create Car model and configure Admin

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from cars.models import Car
 
 # Register your models here.
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    list_filter = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    search_fields = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    ordering = ['model', 'brand', 'factory_year', 'model_year', 'value']
+    fields = ('model', 'brand', 'factory_year', 'model_year', 'value')
+
+admin.site.register(Car, CarAdmin)
