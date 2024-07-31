d212d60f972b8c5ef453f70034387ff53c7b7ae7
Diego Collares <diego.collares@gmail.com>
2024-07-31 17:11:27 -0300

    Create Brand model, add photo Car model and configure media image

--- cars/admin.py
+++ cars/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
 
 # Register your models here.
+
+class BrandAdmin(admin.ModelAdmin):
+  list_display = ('name',)
+  list_filter = ('name',)
+  search_fields = ('name',)
+  ordering = ['name']
+  fields = ('name',)
 
 
 class CarAdmin(admin.ModelAdmin):
@@ -9,6 +16,8 @@ class CarAdmin(admin.ModelAdmin):
     list_filter = ('model', 'brand', 'factory_year', 'model_year', 'value')
     search_fields = ('model', 'brand', 'factory_year', 'model_year', 'value')
     ordering = ['model', 'brand', 'factory_year', 'model_year', 'value']
-    fields = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    fields = ('model', 'brand', 'factory_year', 'model_year', 'value', 'photo')
 
+
+admin.site.register(Brand, BrandAdmin)
 admin.site.register(Car, CarAdmin)
