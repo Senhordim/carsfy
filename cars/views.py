f850647c8a3202c7beca1f533415669d656b8782
Diego Collares <diego.collares@gmail.com>
2024-08-01 10:04:10 -0300

    update template and configure currency number layout

--- cars/views.py
+++ cars/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
 
+from cars.models import Car
+
 def index(request):
-  return render(request, 'index.html', context={
-    'cars': [
-      {
-        'model': 'Toyota Camry',
-        'year': 2022,
-        'price': 25000,
-        'image': 'https://www.toyota.com/imgix/responsive/images/mlp/colorizer/2022/camry/1H1/1.png?bg=fff&fm=webp&w=930',
-      },
-      {
-        'model': 'Honda Accord',
-        'year': 2022,
-        'price': 24000,
-        'image': 'https://www.honda.com/imgix/responsive/images/mlp/colorizer/2022/accord/1H1/1.png?bg=fff&fm=webp&w=930',
-      },
-      {
-        'model': 'Nissan Altima',
-        'year': 2022,
-        'price': 23000,
-        'image': 'https://www.nissanusa.com/imgix/responsive/images/mlp/colorizer/2022/altima/1H1/1.png?bg=fff&fm=webp&w=930',
-      },
-    ]
-  })
+  # cars = Car.objects.filter(brand__name='Fiat')
+  # cars = Car.objects.filter(model__contains='Uno')
+  cars = Car.objects.all()
+  return render(request, 'index.html', context={'cars': cars})
