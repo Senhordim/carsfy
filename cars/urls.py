2bc6a81701b47d495f8a65ef6add076f6599e57c
Diego Collares <diego.collares@gmail.com>
2024-08-02 17:40:33 -0300

    update form and save action view

--- cars/urls.py
+++ cars/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from cars.views import index
+from cars.views import index, new
 
 urlpatterns = [
   path('', index, name="index"),
+  path('new/', new, name="new"),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from cars.models import Car
+from cars.forms import CarForm
 
 def index(request):
   cars = Car.objects.all().order_by('created_at')
@@ -8,3 +8,17 @@ def index(request):
   if search:
     cars = Car.objects.filter(model__icontains=search)
   return render(request, 'index.html', context={'cars': cars})
+
+def new(request):
+  if request.method == 'POST':
+    new_car_form = CarForm(request.POST, request.FILES)
+    if new_car_form.is_valid():
+      new_car_form.save()
+      return redirect('index')
+  if request.method == 'GET':
+    new_car_form = CarForm()
+  return render(request, 'new.html', context={'new_car_form': new_car_form})
+
+
+
+
