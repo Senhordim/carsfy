fd3f9892af4b2670e4ffbf5e33662c07be1be605
Diego Collares <diego.collares@gmail.com>
2024-08-05 19:35:01 -0300

    Auth and register, verify user view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import register, auth
+from .views import register, auth, logout_site
 urlpatterns = [
   path('register', register, name="register"),
   path('auth', auth, name="auth"),
+  path('logout', logout_site, name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
+
+def logout_site(request):
+  logout(request)
+  return redirect('index')
 
 def auth(request):
-  user_form_authentication = AuthenticationForm()
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+      login(request, user)
+      return redirect('index')
+    else:
+      return redirect('auth')
+  else:
+    user_form_authentication = AuthenticationForm()
   return render(request, 'auth.html', context={'user_form_authentication': user_form_authentication})
 
 def register(request):
